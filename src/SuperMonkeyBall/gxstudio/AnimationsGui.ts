0d5b75550563f27d34f08205ef799e920fa85aa6
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2025-08-23 19:07:04 -0400

    More anim gui stuff

--- src/SuperMonkeyBall/gxstudio/AnimationsGui.ts
+++ src/SuperMonkeyBall/gxstudio/AnimationsGui.ts
@@ -2,7 +2,7 @@ import { ImGui, ImVec2 } from "@mori2003/jsimgui";
 import { GfxDevice } from "../../gfx/platform/GfxPlatform";
 import { GfxRenderCache } from "../../gfx/render/GfxRenderCache";
 import { TextureCache } from "../ModelCache";
-import { ColorAnim, ColorChannel, InterpKind, Material, Model, ScalarAnim, ScalarChannel, Texture } from "./Scene";
+import { ColorAnim, ColorChannel, Interp, InterpKind, Material, Model, ScalarAnim, ScalarChannel, Texture } from "./Scene";
 import { MaterialListGui } from "./MaterialListGui";
 import { OpaqueBlack } from "../../Color";
 import { createIdMap, renderCombo } from "./GuiUtils";
@@ -16,6 +16,11 @@ type ColorChannelInfo = {
     id: ColorChannel,
     label: string,
 };
+
+type InterpKindInfo = {
+    id: InterpKind,
+    label: string,
+}
 
 const SCALAR_CHANNELS: ScalarChannelInfo[] = [
     { id: ScalarChannel.UV0_TranlateU, label: "TexCoord0 Translate U" },
@@ -48,8 +53,18 @@ const COLOR_CHANNELS: ColorChannelInfo[] = [
     { id: ColorChannel.CPREV, label: "Color Register 'PREV'" },
 ];
 
+const INTERP_KINDS: InterpKindInfo[] = [
+    { id: InterpKind.Constant, label: "Constant" },
+    { id: InterpKind.Linear, label: "Linear" },
+    { id: InterpKind.Sine, label: "Sine" },
+    { id: InterpKind.Square, label: "Square" },
+];
+
 const SCALAR_CHANNEL_MAP = createIdMap(SCALAR_CHANNELS);
 const COLOR_CHANNEL_MAP = createIdMap(COLOR_CHANNELS);
+const INTERP_KIND_MAP = createIdMap(INTERP_KINDS);
+
+const scratchNumberPtr = [0];
 
 export class AnimationsGui {
     constructor(
@@ -141,6 +156,15 @@ export class AnimationsGui {
                 (c) => c.label,
             ).id;
 
+            anim.interp.kind = renderCombo(
+                "Interp",
+                INTERP_KINDS,
+                INTERP_KIND_MAP.get(anim.interp.kind)!,
+                (i) => i.label,
+            ).id;
+
+            this.renderInterp(anim.interp);
+
             if (ImGui.Button("Delete")) {
                 deleteMe = true;
             }
@@ -164,6 +188,15 @@ export class AnimationsGui {
                 (c) => c.label,
             ).id;
 
+            anim.interp.kind = renderCombo(
+                "Interp",
+                INTERP_KINDS,
+                INTERP_KIND_MAP.get(anim.interp.kind)!,
+                (i) => i.label,
+            ).id;
+
+            this.renderInterp(anim.interp);
+
             if (ImGui.Button("Delete")) {
                 deleteMe = true;
             }
@@ -172,5 +205,22 @@ export class AnimationsGui {
         }
        
         return deleteMe;
+    }
+
+    private renderInterp(interp: Interp) {
+        const n = scratchNumberPtr;
+        if (interp.kind !== InterpKind.Constant) {
+            n[0] = interp.offset;
+            ImGui.SliderFloat("Offset", n, 0, 1);
+            interp.offset = n[0];
+
+            n[0] = interp.scale;
+            ImGui.SliderFloat("Scale", n, -10, 10);
+            interp.scale = n[0];
+            
+            n[0] = interp.speed;
+            ImGui.SliderFloat("Speed", n, -10, 10);
+            interp.speed = n[0];
+        }
     }
 }
